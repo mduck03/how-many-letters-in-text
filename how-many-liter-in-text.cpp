df1e94f19f43b700c2d7c8a553850ea6ef1c7afe
mduck03 <61601954+mduck03@users.noreply.github.com>
2020-12-07 20:58:07 +0100

    fully warking program

---
+++ how-many-liter-in-text.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    char text[100000];
+    cout << "type text : ";
+    cin >> text;
+    char test;
+    cout << "letter are you looking for:";
+    cin >> test;
+    int end=0;
+    for (int i = 0; i < 10000; i++)
+    {
+        if (text[i] == test) {
+            end++;
+        }
+    }
+    cout << '"' << test << '"' << " appeared " << end << " times\n";
+    cout <<"type enything to stop program";
+    cin >> test;
+}
